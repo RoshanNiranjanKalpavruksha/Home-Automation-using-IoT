e5d9937a600e15dfa0898022da9c9eba4602f2d3
roshannkalpavruksha8585 <rkalpavruksha@yahoo.com>
2024-03-09 22:07:41 -0500

    code_push
    code_push

---
+++ Code - Home Automation using Iot/ubi_sub.ino
@@ -0,0 +1,78 @@
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include "UbidotsESPMQTT.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define TOKEN "BBFF-0OtimNCB4iZm5qLF0KhrQOZAPMrand" // Your Ubidots TOKEN
+#define WIFINAME "Rohan" //Your SSID
+#define WIFIPASS "1234556789" // Your Wifi Pass
+#define triacPulse 4 //D2
+#define ZVC 12 //D6
+
+Ubidots client(TOKEN);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+int val;
+
+void ICACHE_RAM_ATTR acon()
+{
+  // Serial.println("REad");
+   
+  delayMicroseconds(val); // read AD0
+  digitalWrite(triacPulse, HIGH);
+  
+  // Serial.println(digitalRead(triacPulse));
+  
+  delayMicroseconds(50);  //delay 50 uSec on output pulse to turn on triac
+  digitalWrite(triacPulse, LOW);
+  
+  // Serial.println(digitalRead(triacPulse));
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  int j= length-1;
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+    int mymsg = atoi ((const char*)payload);
+    val=mymsg;
+  }
+  //Serial.print(val);
+  val=map(val,0,100,7200,200);
+  Serial.println();
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  client.setDebug(true); // Pass a true or false bool value to activate debug messages
+  client.wifiConnection(WIFINAME, WIFIPASS);
+  client.begin(callback);
+  client.ubidotsSubscribe("nodemcu","bulb"); //Insert the dataSource and Variable's Labels
+
+   pinMode(ZVC, INPUT_PULLUP);
+  //digitalWrite(2, INPUT_PULLUP); // pull up
+  pinMode(triacPulse, OUTPUT);
+  attachInterrupt(ZVC, acon, FALLING); // attach Interrupt at PIN2
+  
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()){
+      client.reconnect();
+      client.ubidotsSubscribe("nodemcu","bulb"); //Insert the dataSource and Variable's Labels
+      }
+  client.loop();
+  }
---
+++ Final Presentation/code/mpu_ubi/mpu_ubi.ino
@@ -0,0 +1,137 @@
+#include "UbidotsESPMQTT.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+int ax, ay, az;
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define TOKEN "BBFF-F8yVMuPkZ0prU48KOF5GJr5BG3X1Oh" // Your Ubidots TOKEN
+#define WIFINAME "Rohan" //Your SSID
+#define WIFIPASS "1234556789" // Your Wifi Pass
+
+Ubidots client(TOKEN);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  // put your setup code here, to run once:
+  //client.ubidotsSetBroker("industrial.api.ubidots.com"); // Sets the broker properly for the industrial account
+  client.setDebug(true); // Pass a true or false bool value to activate debug messages
+  Serial.begin(115200);
+  client.wifiConnection(WIFINAME, WIFIPASS);
+  client.begin(callback);
+
+   Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()){
+      client.reconnect();
+      }
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+ax = map(a.acceleration.x, -10, 10, 0, 100);             // AY = Y AXIS AND Y = 40 TO 77
+
+ay = map(a.acceleration.y,-9,9,0,100);
+  
+  //float value1 = random(0,100);
+  if (ax>30 && ax <70)
+{
+  client.add("Bulb", ay);
+  client.ubidotsPublish("NodeMCU");
+}
+  client.loop();
+}
